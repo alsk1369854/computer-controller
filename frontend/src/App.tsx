55f0e60526896f40d55d5e5bbd7e47294e892333
ChiaMing <alsk1369854@gmail.com>
2024-03-29 13:22:40 +0800

    feat: 安裝 antd 依賴

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
 
+import { Button } from "antd";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button type="primary">PRESS ME</Button>
+    </div>
+  );
 }
 
 export default App;
